3825ec267daf5109babbe6279ec2aa33cc75472a
sean.wong <sean.wong@zeasn.com>
2018-07-27 14:21:20 +0800

    first complete

---
+++ server.js
@@ -0,0 +1,101 @@
+const fs=require("fs");
+const path=require("path");
+const http = require("http");
+const ejs = require("ejs");
+const config = {
+	port:1111
+}
+let root = null;
+
+const getFileExt = (fileName) => {
+	let arr = fileName.split(".");
+	return arr[arr.length - 1];
+}
+
+const isFile = (path) => {
+	return fs.existsSync(path)&&fs.statSync(path).isFile();
+}
+
+if (process.argv[2]) {
+	root = process.argv[2];
+} else {
+	root = __dirname;
+}
+
+const router = (req, res) => {
+	if (req.url ==="/favicon.ico") {
+		return;
+	}
+	if (req.url === "/") {
+		return handleDir(root,req, res);
+	}
+	if (req.url !== "/") {
+		// 真实的绝对地址
+		let reqPath = path.join(root, req.url);
+		if (reqPath.indexOf('?')>0) {
+			reqPath = reqPath.split('?')[0];
+		}
+		reqPath = decodeURI(reqPath);
+		if (isFile(reqPath)) {
+			return handleFile(reqPath,req, res);
+		} else {
+			return handleDir(reqPath,req, res);
+		}
+	}
+}
+
+// handle different directories
+const handleDir = (dir, req, res) => {
+	let arr = fs.readdirSync(dir), datas = [];
+	for (var i = 0; i < arr.length; i++){
+		let filePath = path.join(dir, arr[i]);
+		datas.push({ name: arr[i], isFile: isFile(path.resolve(dir, arr[i])), path: path.relative(root, filePath)});
+	}
+	let startInd = 0, dirs = [], relPath = path.relative(root, dir), endInd = relPath.indexOf('\\');
+	while (endInd > 0) {
+		dirs.push({ name: relPath.substring(startInd, endInd),path:"/"+relPath.substring(0, endInd).replace(/\\\\/ig,"/")});
+		startInd = endInd+1;
+		endInd = relPath.indexOf('\\', startInd);
+	}
+	dirs.push({ name: relPath.substring(startInd), path: "/" + relPath.replace(/\\\\/ig, "/") });
+	const data=fs.readFileSync("./index.ejs").toString();
+	const html = ejs.render(data, {folders:dirs, datas: datas });
+	res.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
+	res.end(html);
+}
+
+// handle different files
+const handleFile = (file, req, res) => {
+	const fileType = getFileExt(file);
+	let mimeType = '';
+	switch (fileType) {
+		case 'css':
+			mimeType='text/css';
+			break;
+		case 'jpg':
+			mimeType = 'image/jpeg';
+			break;
+		case 'js':
+			mimeType = 'application/x-javascript';
+			break;
+		case 'jpg':
+			mimeType = 'image/jpeg';
+			break;
+		case 'png':
+			mimeType = 'image/png';
+			break;
+		case 'gif':
+			mimeType = 'image/gif';
+			break;
+	}
+	const data = fs.readFileSync(file);
+	res.writeHead(200, { "Content-Type": mimeType })
+	res.end(data);
+}
+
+const server = http.createServer(router).listen(config.port, (err) => {
+	if (err) {
+		throw new Error(err);
+	}
+	console.log("Server is running on port " + config.port);
+});
